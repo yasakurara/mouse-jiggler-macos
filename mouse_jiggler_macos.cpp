983ff0f9fbc88540b2ba14f52d2538914f0ed76e
yasakura <47058814+yasakurara@users.noreply.github.com>
2022-10-05 23:22:04 +0900

    enhanced

--- mouse_jiggler_macos.cpp
+++ mouse_jiggler_macos.cpp
@@ -1,30 +1,65 @@
-#include <ApplicationServices/ApplicationServices.h>
 #include <iostream>
+#include <ctime>
+#include <random>
 
-int main(void)
-{
-    CGRect displayBounds = CGDisplayBounds(CGMainDisplayID());
+#include <ApplicationServices/ApplicationServices.h>
+
+using namespace std;
+
+void usage(void) {
+    cout << "mouse_jiggler_macos -p <poll[sec]> -t <timeout[sec]> " << endl;
+    exit(1);
+}
+
+int main(int argc, char *argv[]) {
+    int poll_sec = 0;
+    int timeout_sec = 0;
+
+    int c;
+    while ((c = getopt(argc, argv, "?p:t:")) != -1) {
+        switch (c) {
+            case '?': usage();
+            case 'p': poll_sec = atoi(optarg); break;
+            case 't': timeout_sec = atoi(optarg); break;
+        }
+    }
+    if (poll_sec <= 0 || timeout_sec <= 0) usage();
+
     CGPoint preLocation = {};
-    int preX = 0;
+    time_t preLocationTime;
+    random_device rnd;
     while (1) {
-        sleep(60*5);
+        sleep(poll_sec);
+        
         CGEventSourceRef eventSource = CGEventSourceCreate(kCGEventSourceStateHIDSystemState);
         CGEventRef event = CGEventCreate(eventSource);
         CGPoint currentLocation = CGEventGetLocation(event);
-        if (preLocation.x == 0 && preLocation.y == 0) {
+        if (preLocation.x == 0 && preLocation.y == 0) { // init
             preLocation = currentLocation;
+            preLocationTime = time(nullptr);
             continue;
         }
         if (preLocation.x != currentLocation.x || preLocation.y != currentLocation.y) {
             preLocation = currentLocation;
+            preLocationTime = time(nullptr);
             continue;
         }
-        preX = preX ? 0 : 1;
-        CGPoint tgtLocation = CGPointMake(preX, (float)(displayBounds.size.height));
+
+        double diffTime = difftime(time(nullptr), preLocationTime);
+        if (timeout_sec > (int)diffTime) {
+            continue;
+        } 
+        
+        CGRect displayBounds = CGDisplayBounds(CGMainDisplayID());
+        float widthHalf = (float)(displayBounds.size.width)/2;
+        float heightHalf = (float)(displayBounds.size.height)/2;
+        float jiggle = rnd() % 10;
+        CGPoint tgtLocation = CGPointMake(widthHalf+jiggle, heightHalf+jiggle);
         CGEventRef mouseEvent = CGEventCreateMouseEvent(eventSource, kCGEventMouseMoved, tgtLocation, kCGMouseButtonLeft);
         CGEventPost(kCGHIDEventTap, mouseEvent);
         CFRelease(mouseEvent);
         CFRelease(event);
+        preLocation = tgtLocation;
     }
     return 0;
 }
